85f842f4ff9e3bcf2fb29fdd9323968ed551d1c8
SanketPawar-tech <sanketpawarsp324@gmail.com>
2025-11-05 13:26:59 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -12,12 +12,22 @@ app.use(cors({
     credentials: true, 
 
 }))
+
+
 app.use(express.json({limit: "16kb"}));
-
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-
 app.use(express.static("public"));
-
 app.use(cookieParser())
 
-export { app };
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter);    //userRouter used as middleware 
+
+
+//http://localhost:8000/api/v1/users/register
+//http:localhost:8000/users/login
+export default app ;
